812e9155b897d363a8c3b3e3d38aed4ba01de9f1
Martin Zalazar <martinzetav@gmail.com>
2025-07-05 13:02:46 -0300

    se creo la entidad User

---
+++ src/main/java/com/app/foro_hub/model/UserEntity.java
@@ -0,0 +1,29 @@
+package com.app.foro_hub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @ManyToMany
+    @JoinTable(
+            name = "user_profile",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "profile_id")
+    )
+    private List<Profile> profiles;
+}
