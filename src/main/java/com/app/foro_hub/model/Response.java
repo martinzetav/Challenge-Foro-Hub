22b7b2f29408beba860cba9acdfd1ec63196b65a
Martin Zalazar <martinzetav@gmail.com>
2025-07-05 13:02:35 -0300

    se creo la entidad Response

---
+++ src/main/java/com/app/foro_hub/model/Response.java
@@ -0,0 +1,29 @@
+package com.app.foro_hub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "responses")
+public class Response {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private UserEntity author;
+    private String solution;
+}
