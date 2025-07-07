e41c6368ba4e4d4a2440a52e88c6bae40337bfe1
Martin Zalazar <martinzetav@gmail.com>
2025-07-07 09:11:32 -0300

    se agrego anotacion transactional

--- src/main/java/com/app/foro_hub/service/impl/TopicService.java
+++ src/main/java/com/app/foro_hub/service/impl/TopicService.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -28,6 +29,7 @@ public class TopicService implements ITopicService {
     private final IUserEntityRepository userEntityRepository;
 
     @Override
+    @Transactional
     public TopicResponseDTO createTopic(TopicRequestDTO topicRequestDTO) {
         Course course = courseRepository.findById(topicRequestDTO.courseId())
                 .orElseThrow(() -> new ResourceNotFoundException(
